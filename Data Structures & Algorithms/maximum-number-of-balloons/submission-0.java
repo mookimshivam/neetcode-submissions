25c374797338ec9d532fd87e60c5b6218f981e07
Shivam Kumar Mookim <61063462+mookimshivam@users.noreply.github.com>
2026-06-22 13:36:15 +0530

    Add: maximum-number-of-balloons - submission-0

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    // specific for word balloon
+    public int maxNumberOfBalloons(String text) {
+        int b=0, a=0, l=0, o=0, n=0;
+        for(char c: text.toCharArray()){
+            switch(c){
+                case 'b': b++; break;
+                case 'a': a++; break;
+                case 'l': l++; break;
+                case 'o': o++; break;
+                case 'n': n++; break;
+            }
+        }
+        l = l/2;
+        o = o/2;
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
+    }
+}
